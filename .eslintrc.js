bb9ed89bfa873d6495eecfe398d28399c3a86821
LiuXiaoying <2214872361@qq.com>
2022-10-16 09:10:46 +0800

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,30 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/vue3-essential", "@vue/standard"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    quotes: [
+      2,
+      "single",
+      {
+        avoidEscape: false,
+        allowTemplateLiterals: true,
+      },
+    ],
+    semi: [2, "never"],
+    "semi-spacing": [
+      2,
+      {
+        before: false,
+        after: true,
+      },
+    ],
+  },
+};
